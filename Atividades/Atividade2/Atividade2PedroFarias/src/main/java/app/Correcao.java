df9ad044602a2c3e7749df30faeefd9302099622
Pedro Farias <pedroboscachi@gmail.com>
2022-11-08 20:16:24 -0300

    corrigido erro de persistencia

--- Atividades/Atividade2/Atividade2PedroFarias/src/main/java/app/Correcao.java
+++ Atividades/Atividade2/Atividade2PedroFarias/src/main/java/app/Correcao.java
@@ -2,7 +2,6 @@ package app;
 
 import java.util.List;
 
-import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -12,9 +11,10 @@ import entidades.Filme;
 public class Correcao {
 
 	public static void main(String[] args) {
-		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("persistence");
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("Atividade2PedroFarias");
 		EntityManager entidade = fabrica.createEntityManager();
 		List<Filme> filmes = entidade.createQuery("FROM Filme f join fetch f.diretor", Filme.class).getResultList();
+		System.out.println(filmes);
 	}
 
 }
